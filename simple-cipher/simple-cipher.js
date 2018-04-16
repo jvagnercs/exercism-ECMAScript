5e74c7a311af0f3d5e059ce6502d97441a28381e
jvagnercs <jvagnercs@github.com>
2018-04-16 16:03:18 -0300

    2nd iteration, fine on html file, but class implementation still pending

--- simple-cipher/simple-cipher.js
+++ simple-cipher/simple-cipher.js
@@ -2,70 +2,85 @@ class Cipher {
   constructor(key) {
     this.key = key
   }
+
+  LOWAY = "a"
+  LOWZED = "z"
+  HIGHAY = "A"
+  HIGHZED = "Z"
+
+  checkLower = c => c >= LOWAY && c <= LOWZED
+  checkUpper = c => c >= HIGHAY && c <= HIGHZED
     
-  decode() {
-    // console.log()
-  }
-       
-  encode() {
-    const LOWAY = "a"
-    const LOWZED = "z"
-    const HIGHAY = "A"
-    const HIGHZED = "Z"
-
+  encode = unCiphered => {
     // proceeds only if input is not null
-    if (unCiphered === null || unCiphered === undefined) return "\nProper usage:\n\tcaesar('string')\n"
-
+    if (unCiphered === null || unCiphered === undefined) return "\nProper usage:\n\tencode('string')\n"
+  
     let ciphered = []
-
+  
     // loops through the string
     for (let key in unCiphered) {
+      const individualChar = unCiphered[key]
       // check if the char is either lower or upper case
-      if(checkLower(unCiphered[key]) || checkUpper(unCiphered[key])) {
-        // should that be the case, toCaesar is called to encrypt this single char
-        ciphered.push(String.fromCharCode(toCaesar(unCiphered[key])))
+      if(checkLower(individualChar) || checkUpper(individualChar)) {
+        // cipherChar is called to encrypt this single char
+        const cipheredChar = cipherChar(individualChar)
+        // push it into array
+        ciphered.push(String.fromCharCode(cipheredChar))
       } else {
         // if it is a number, or symbol, it is put into the array outright
-        ciphered.push(unCiphered[key])
+        ciphered.push(individualChar)
       }
     }
     // prints the now encrypted string
-    console.log('ciphered',ciphered.join(''))
+    return ciphered.join('')
+  }
+       
+  decode = ciphered => {
+    if (ciphered === null || ciphered === undefined) return "\nProper usage:\n\tdecode('string')\n"
+  
+    let deciphered = []
+    for (let key in ciphered) {
+      if(checkLower(ciphered[key]) || checkUpper(ciphered[key])) {
+        deciphered.push(String.fromCharCode(deCipherChar(ciphered[key])))
+      } else {
+        deciphered.push(ciphered[key])
+      }
+    }
+    return deciphered.join('')
   }
 
-  const checkLower = c => c >= LOWAY && c <= LOWZED
-  const checkUpper = c => c >= HIGHAY && c <= HIGHZED
-
-  const toCaesar = c => {
-    let upper_bound, lower_bound
-      
-    // sets values for variables, according to the char case
+  getBoundaries = c => {
+    let charBoundaries = []
     if (checkLower(c)) {
-      lower_bound = LOWAY
-      upper_bound = LOWZED
-    }
-    else {
-      lower_bound = HIGHAY
-      upper_bound = HIGHZED
-    }
-    
-    let offset = 3
-
-    // gets the sum of char plus the offset
-    let char_offset = offset + c.charCodeAt(0)
-
-    // discards the part above upper_limit and gets the modulo into cut_off
-    let cut_off = char_offset % (upper_bound.charCodeAt(0) + 1)
-
-    // if cut_off is less than lower_bound
-    if (cut_off < lower_bound.charCodeAt(0)) {
-      // sum both to avoid a number less than lower_bound
-      return cut_off + lower_bound.charCodeAt(0)
+      charBoundaries.push(LOWAY.charCodeAt(0))
+      charBoundaries.push(LOWZED.charCodeAt(0))
     } else {
-      // returns cut_off
-      return cut_off
+      charBoundaries.push(HIGHAY.charCodeAt(0))
+      charBoundaries.push(HIGHZED.charCodeAt(0))
+    }
+    return charBoundaries
+  }
+  
+  cipherChar = c => {
+    const [lower_bound, upper_bound] = getBoundaries(c)
+    const offset = 3
+    const char_offset = offset + c.charCodeAt(0)
+    if (char_offset > upper_bound) {
+      return lower_bound + char_offset - (upper_bound + 1)
+    } else {
+      return char_offset
     }
   }
-
+  
+  deCipherChar = c => {
+    const [lower_bound, upper_bound] = getBoundaries(c)
+    const offset = -3
+    const char_offset = offset + c.charCodeAt(0)
+    if (char_offset < lower_bound) {
+      return upper_bound - (lower_bound - 1 - char_offset)
+    } else {
+      return char_offset
+    }
+  }
 }
 
